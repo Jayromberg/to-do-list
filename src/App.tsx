53d231478e69249386506a744e5ebe5c49cdae0d
Jayromberg <jayro.dev@gmail.com>
2022-10-13 11:28:13 -0300

    feat: Implementado um button de teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from './components/button';
 
 function App() {
   return (
     <div className="App">
      Hello Word!
+     <Button />
     </div>
   );
 }
