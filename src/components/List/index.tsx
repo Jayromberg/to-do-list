53788f362d93aa2233fd6ffb2b0fb614135a9135
Jayromberg <jayro.dev@gmail.com>
2022-10-15 11:54:33 -0300

    refactor: Implementado os estados na aplicação

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,24 +1,25 @@
 import { Lists } from './styles';
 import Item from './Item';
+import { useState } from 'react';
 
 function List() {
-  const tarefas = [{
+  const [tasks, setTasks] = useState([{
     tarefa: "React",
     tempo: "03:00:00"
   },
   {
     tarefa: "TypeScript",
     tempo: "03:00:00"
-  }]
+  }])
 
   return (
     <Lists>
       <h2>Tarefas do dia</h2>
       <ul>
-       {tarefas.map((e, index) => (
+       {tasks.map((task, index) => (
         <Item
-        key={`${index}${e.tarefa}`} 
-        {...e}
+        key={`${index}${task.tarefa}`} 
+        {...task}
         />
        ))}
       </ul>
