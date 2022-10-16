5e33cc2810359ba102e77b9926fcc8ee7a9097d7
Jayromberg <jayro.dev@gmail.com>
2022-10-15 23:20:16 -0300

    feat: Inicio da implementação do time

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,8 +1,21 @@
 import Button from "../Button";
 import Clock from "./Clock";
 import TimerStyle from "./styles";
+import { timeToSeconds } from "../../common/utils/time";
+import { useContext, useEffect, useState } from "react";
+import GlobalStateContext from "../../contexts/GlobalStateContext";
 
 export default function Timer() {
+  const { selected } = useContext(GlobalStateContext);
+
+  const [time, setTime] = useState<number>();
+
+  useEffect(() => {
+    if(selected?.time) {
+      setTime(timeToSeconds(selected.time));
+    }
+  }, [selected])
+
   return(
     <TimerStyle>
       <p className="titulo">Escolha um card e inicie o cronômetro</p>
